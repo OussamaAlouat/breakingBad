92aeea62751620f3433c0d5b42b4722eb6c2b2e0
OussamaAlouat <al317257@uji.es>
2023-03-14 21:12:01 +0100

    FEAT: add new component

---
+++ src/characters/components/CardList.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import breakingBadApi from '@/api/breakingBadApi';
+
+  breakingBadApi.get('/quotes').then((resp) => console.log(resp.data))
+</script>
+
+<template>
+  <ul>
+    <li>Hola</li>
+    <li>Hola</li>
+    <li>Hola</li>
+  </ul>
+</template>
+
+<style scoped>
+</style>
