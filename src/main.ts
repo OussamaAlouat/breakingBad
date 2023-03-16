14afbef9f9ceed88ae139fe1c916a16386a08f99
OussamaAlouat <al317257@uji.es>
2023-03-16 21:46:23 +0100

    FEAT: Added vueQuery

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 import './assets/main.css';
 
 const app = createApp(App);
+
+app.use(VueQueryPlugin);
 
 app.use(router);
 
