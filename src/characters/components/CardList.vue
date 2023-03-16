01a2fe2e17b7086fbb22b49324856ee09692c268
OussamaAlouat <al317257@uji.es>
2023-03-16 21:33:12 +0100

    FIX: change composable and control errors

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -3,18 +3,17 @@ import rickAndMortyApi from '@/api/rickAndMortyApi';
 import type { RickAndMorty } from '@/characters/interfaces/character';
 import { useCharacters } from '../composables/useCharacters';
 
-const { characters, isLoading } = useCharacters();
+const { characters, isLoading, hasError, errorMessage } = useCharacters();
 
 
 </script>
 
 <template>
-
   <h1 v-if="isLoading">Loading ....</h1>
+  <h1 v-if="hasError">Error: {{ errorMessage }}</h1>
   <ul>
-    <li v-for="character of characters" >{{  character.name  }}</li>
+    <li v-for="character of characters">{{ character.name }}</li>
   </ul>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/characters/composables/useCharacters.ts
+++ src/characters/composables/useCharacters.ts
@@ -1,19 +1,43 @@
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import rickAndMortyApi from "@/api/rickAndMortyApi";
 import type { Character, RickAndMorty } from "../interfaces/character";
+import axios from "axios";
+
+const characters = ref<Character[]>([]);
+const isLoading = ref<boolean>(true);
+const errorMessage = ref<string>('');
+const hasError = ref<boolean>(false);
 
 export const useCharacters = () => {
-  const characters = ref<Character[]>([]);
-  const isLoading = ref<boolean>(true);
 
-  rickAndMortyApi.get<RickAndMorty>('/character')
-    .then((resp) =>{
-      characters.value = resp.data.results;
-      isLoading.value = false;
+  onMounted(async() => {
+    loadCharacters();
   });
+
+  const loadCharacters = async () => {
+    if(characters.value.length > 0) return;
+    isLoading.value = true;
+    try {
+      const { data } = await  rickAndMortyApi.get<RickAndMorty>('/character');
+      characters.value = data.results;
+      isLoading.value = false;
+    } catch (error) {
+      isLoading.value = false;
+      hasError.value = true;
+
+
+      if(axios.isAxiosError(error) ) {
+        return errorMessage.value = error.message;
+      }
+
+      errorMessage.value = JSON.stringify(error);
+    }
+  }
 
   return {
     characters,
     isLoading,
+    errorMessage,
+    hasError
   };
 }
