57856fe14f91d08e906aa340ed4fa6f713fc086f
OussamaAlouat <al317257@uji.es>
2023-03-17 14:07:38 +0100

    FEAT: on click on a card, goes to characterId

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import type { Character } from '../interfaces/character';
+
+const route = useRoute();
+const { id} = route.params as { id: string };
+
+
 </script>
 
 <template>
   <div>
-    <h1>CharacterId</h1>
+    <h1>Character {{ id }}</h1>
+    <!--
+    <div>
+      <h1>{{ character.name }}</h1>
+      <img :src="character.image" :alt="character.name" >
+    </div>
+    -->
   </div>
 </template>
 
