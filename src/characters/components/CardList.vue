d0bdeb871d19e940382d0839ad21c2e7bdd1216a
OussamaAlouat <al317257@uji.es>
2023-03-17 10:56:14 +0100

    FIX: extract http from cardList to characterList

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,29 +1,11 @@
 <script setup lang="ts">
 import CharacterCard from '@/characters/components/CharacterCard.vue';
-import { useQuery } from '@tanstack/vue-query';
-import rickAndMortyApi from '@/api/rickAndMortyApi';
-import type { Character, RickAndMorty } from '@/characters/interfaces/character';
+import type { Character } from '@/characters/interfaces/character';
 
-
-const getCharactersSlow = async(): Promise<Character[]> => {
-  const { data} = await rickAndMortyApi.get<RickAndMorty>('/character');
-  return data.results;
-}
-
-const { isLoading, isError, data: characters, error } = useQuery(
-  ['characters'],
-  getCharactersSlow,
-  {
-    cacheTime: 1000 * 60, // cache for a 1 minute time
-    refetchOnReconnect: 'always',
-  }
-);
-
+const props = defineProps<{ characters: Character[] }>()
 </script>
 
 <template>
-  <h1 v-if="isLoading">Loading ....</h1>
-  <h1 v-if="isError">Error: {{ error }}</h1>
   <div class="card-list">
     <CharacterCard
       v-for="character of characters"
--- src/characters/pages/CharacterList.vue
+++ src/characters/pages/CharacterList.vue
@@ -1,15 +1,32 @@
 <script setup lang="ts">
 import CardList from '@/characters/components/CardList.vue';
+import type { Character, RickAndMorty } from '@/characters/interfaces/character';
+import rickAndMortyApi from '@/api/rickAndMortyApi';
+import { useQuery } from '@tanstack/vue-query';
 
 const props = defineProps<{ title: string, visible: boolean }>()
+
+const getCharacters = async (): Promise<Character[]> => {
+  const { data } = await rickAndMortyApi.get<RickAndMorty>('/character');
+  return data.results;
+}
+
+const { isLoading, isError, data: characters, error } = useQuery(
+  ['characters'],
+  getCharacters
+);
+
 </script>
 
 <template>
   <div>
-    <h2>{{ props.title }}</h2>
-    <CardList />
+    <h1 v-if="isLoading">Loading ....</h1>
+    <template v-else>
+      <h2>{{ props.title }}</h2>
+      <h1 v-if="isError">Error: {{ error }}</h1>
+      <CardList v-if="characters" :characters="characters" />
+    </template>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
