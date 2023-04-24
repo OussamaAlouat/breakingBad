17a2c6ea406f00adbaf6529cf669e0ed3f430858
OussamaAlouat <al317257@uji.es>
2023-04-24 18:43:18 +0200

    FEAT: added composable to CharactersList

--- src/characters/pages/CharacterList.vue
+++ src/characters/pages/CharacterList.vue
@@ -1,49 +1,20 @@
 <script setup lang="ts">
 import CardList from '@/characters/components/CardList.vue';
-import characterStore from '@/store/characters.store';
-
-import type { Character, RickAndMorty } from '@/characters/interfaces/character';
-import rickAndMortyApi from '@/api/rickAndMortyApi';
-import { useQuery } from '@tanstack/vue-query';
-import { isAxiosError } from 'axios';
+import useCharacters from '@/characters/composables/userCharacters';
 
 const props = defineProps<{ title: string, visible: boolean }>()
 
-const getCharactersCacheFirst = async (): Promise<Character[]> => {
-  if (characterStore.characters.count > 0) {
-    return characterStore.characters.list;
-  }
-
-  const { data } = await rickAndMortyApi.get<RickAndMorty>('/character');
-  return data.results;
-}
-
-useQuery(
-  ['characters'],
-  getCharactersCacheFirst,
-  {
-    onSuccess(data: Character[]) {
-      characterStore.loadedCharacters(data);
-    },
-    onError(error) {
-      if (isAxiosError(error)) {
-        characterStore.loadedCharactersFailed(error.message);
-      } else {
-        characterStore.loadedCharactersFailed(JSON.stringify(error));
-      }
-    }
-  }
-);
+const { isLoading, characters, errorMessage, hasError, count } = useCharacters();
 
 </script>
 
 <template>
   <div>
-    <h1 v-if="characterStore.characters.isLoading">Loading ....</h1>
+    <h1 v-if="isLoading">Loading ....</h1>
     <template v-else>
-      <h2>{{ props.title }}</h2>
-      <h1 v-if="characterStore.characters.hasError">Error: {{ characterStore.characters.errorMessage }}</h1>
-      <CardList :characters="characterStore.characters.list" />
+      <h2>{{ props.title }} - {{ count }}</h2>
+      <h1 v-if="hasError">Error: {{ errorMessage }}</h1>
+      <CardList :characters="characters" />
     </template>
   </div>
 </template>
